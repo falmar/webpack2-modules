5e45d48538de1c9aed89119614a716f5f3f23354
David Lavieri <daviddlavier@gmail.com>
2017-03-09 00:03:26 -0400

    Add images

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ const config = {
         loader: 'css-loader'
       }),
       test: /\.css$/
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/,
+      use: [{
+        loader: 'url-loader',
+        options: { limit: 40000 }
+      },
+        'image-webpack-loader'
+      ]
     }]
   },
   plugins: [
